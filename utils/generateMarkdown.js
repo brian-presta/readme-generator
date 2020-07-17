7439fd820d13dd25fa8e3da562c489943338b7d3
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-16 18:48:48 -0700

    fixed bug introduced by previous bugfix

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,9 +2,9 @@
 function generateMarkdown(data) {
   // we use a regular expression to replace the whitespace in the license string with %20, 
   // this prevents the shields.io URL from breaking
-  data.license = data.license.replace(/\s/g,'%20')
+  data.licenseURL = data.license.replace(/\s/g,'%20')
   return `# ${data.title}
-  ![license badge](https://img.shields.io/badge/license-${data.license}-green)
+  ![license badge](https://img.shields.io/badge/license-${data.licenseURL}-green)
   ## Description 
 
   ${data.description}
